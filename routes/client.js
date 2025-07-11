3e0184413d6159cecf6dc1396b123fe01d1fdef9
johnyavtheboss <johnyav601@gmail.com>
2025-07-11 12:30:29 +0200

    tries commit

--- routes/client.js
+++ routes/client.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
+const { getClient } = require('../controllers/client');
 
 const router = Router();
+
+router.get('/client/getAllclient');
+router.get('/client/get/:id', getClient);
 
 
 module.exports = router;
